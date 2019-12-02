259b1f763a18ebc34094801fbf640e03b344b6c6
BhaskarShelar <bhaskar.shelar99@gmail.com>
2019-12-02 09:56:25 +0530

    using switch handler function

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1> This Is React learning Course</h1>
-      <h2>APP inside React</h2>
-      <button>Swicth Name</button>
-      <Person name ="bhaskar" age="28"/>
-      <Person name ="vaibhav" age="27">My Hobbied:Racing </Person>
-      <Person name ="prafulla" age="28"/>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    person: [
+      { name: 'bhaskar', age: 28 },
+      { name: 'Prafulla', age: 29 },
+      { name: 'pranita', age: 27 },
+      { name: 'vaibhav', age: 27 }
+    ]
+  };
+
+  switchNameHandler=(newName) => {
+       //Don't Use this this.state.person[0].name="suresh";
+   this.setState({ person: [
+    { name: newName, age: 34 },
+    { name: 'pama', age: 35 },
+    { name: 'Amol', age: 30 },
+    { name: 'vaibhav', age: 27 }
+  ]})
+
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1> This Is React learning Course</h1>
+        <h2>APlication inside React</h2>
+        <button onClick={()=>this.switchNameHandler('Shiva')}>Switch Name</button>
+        <Person
+           name={this.state.person[0].name} 
+           age={this.state.person[0].age}
+           click={this.switchNameHandler.bind(this,'Mummy')}
+           />
+        <Person 
+            name={this.state.person[1].name} 
+            age={this.state.person[1].age}
+            click={this.switchNameHandler.bind(this,'Daddy')}
+            >My Hobbied:Racing </Person>
+        <Person 
+            name={this.state.person[2].name}
+             age={this.state.person[2].age}
+              />
+        <Person 
+            name={this.state.person[3].name} 
+            age={this.state.person[3].age} />
+           
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p> I'm in {props.name} and i {props.age} X Years old!</p>
+            <p onClick={props.click}> I'm in {props.name} and i {props.age} X Years old!</p>
             <p>{props.children}</p>
         </div>
     )    
